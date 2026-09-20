27738cb7e5f91b3e5812c7773fc0e2638d68f824
viper404 <142692522+jwise13804@users.noreply.github.com>
2026-09-20 08:59:28 +0100

    Delete repair-shop directory

--- repair-shop/app/(auth)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="min-h-screen bg-bg flex items-center justify-center p-4">
-      {children}
-    </main>
-  )
-}
